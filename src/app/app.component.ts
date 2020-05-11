854a1b4cae3383471eb7bf83629acadf2bb97920
erenoa123 <brack.lost.barn@gmail.com>
2020-05-11 18:38:09 +0900

    自作アコーディオン

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import {Router, NavigationStart} from '@angular/router';
 export class AppComponent {
   title = 'angular-app';
   message = '';
+  visible:boolean;
 
   constructor(private router:Router){
     router.events.subscribe(event => {
@@ -18,12 +19,20 @@ export class AppComponent {
     });
   }
 
+  ngOnInit(){
+    this.visible = true;
+  }
+
   navigate(event){
     this.message = event.url;
   }
 
   doClick(){
     this.router.navigate(['']);
+  }
+
+  showList(){
+    this.visible = !this.visible;
   }
 
 }
